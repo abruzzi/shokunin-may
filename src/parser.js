4c0c4b2c8a88af3cc4ad8de353847265b3f2fc1a
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-16 20:26:16 +1000

    reshape the data

--- src/parser.js
+++ src/parser.js
@@ -2,7 +2,21 @@ import config from './sensor-group-location.json';
 
 const parse = (data) => {
   const match = config.find(x => new RegExp(x.sensor_uuid_starts_with).test(data.sensor_uuid));
-  return match;
+  
+  return {
+    groupName: match.group_name,
+    timestamp: data.timestamp,
+    location: {
+      latitude: match.latitude,
+      longitude: match.longitude,
+    },
+    readings: {
+      temperature: Number(data.ambient_temperature),
+      humidity: Number(data.humidity),
+      photo: Number(data.photosensor),
+      radiation: Number(data.radiation_level)
+    }
+  };
 }
 
 export {parse};
--- src/parser.test.js
+++ src/parser.test.js
@@ -12,6 +12,33 @@ describe('parser', () => {
     }
 
     const parsed = parse(data);
-    expect(parsed.group_name).toEqual('group_10');
-  })
+    expect(parsed.groupName).toEqual('group_10');
+  });
+
+  it('reshape the data', () => {
+    const data = {
+      ambient_temperature: "23.22",
+      humidity: "76.9735",
+      photosensor: "766.49",
+      radiation_level: "197",
+      sensor_uuid: "probe-c7625902",
+      timestamp: 1557999820
+    }
+
+    const parsed = parse(data);
+    expect(parsed).toEqual(expect.objectContaining({
+      groupName: expect.stringContaining('group_10'),
+      timestamp: 1557999820,
+      location: {
+        latitude: -31.95866,
+        longitude: 115.858317,
+      },
+      readings: {
+        temperature: 23.22,
+        humidity: 76.9735,
+        photo: 766.49,
+        radiation: 197
+      }
+    }))
+  });
 })
