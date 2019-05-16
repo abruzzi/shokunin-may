f2ccf62fd65649b79a9928119c7affb18fa0a641
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-16 20:06:52 +1000

    setup jest and babel

---
+++ src/parser.test.js
@@ -0,0 +1,17 @@
+import {parse} from './parser';
+
+describe('parser', () => {
+  it('group dat by sensor-id', () => {
+    const data = {
+      ambient_temperature: "23.22",
+      humidity: "76.9735",
+      photosensor: "766.49",
+      radiation_level: "197",
+      sensor_uuid: "probe-c7625902",
+      timestamp: 1557999820
+    }
+
+    const parsed = parse(data);
+    expect(parsed.groupId).toEqual('group_10');
+  })
+})
