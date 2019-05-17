f1cd45c272b3291f9a3f9a69e7882bd3132d6ca8
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-17 20:16:46 +1000

    refactor the panel

--- src/ui/Panel.jsx
+++ src/ui/Panel.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  
   .figure {
     flex: 1;
     padding: 10px;
@@ -13,16 +14,20 @@ const Container = styled.div`
 Container.displayName = 'Container';
 
 const format = (figure) => isNaN(Number(figure)) ? '-' : Number(figure).toFixed(2);
-const Panel = ({ 
-    temperature = 0,
-    humidity = 0,
-    light = 0,
-    radiation = 0
-}) => (<Container>
-      <div className="figure temperature">Temperature: {format(temperature)}</div>
-      <div className="figure humidity">Humidity: {format(humidity)}</div>
-      <div className="figure light">Light: {format(light)}</div>
-      <div className="figure radiation">Radiation: {format(radiation)}</div>
+
+const figure = (type, value) => <div key={type} className={`figure ${type.toLowerCase()}`}>{type}: {format(value)}</div>
+
+const Labels = [
+  'Temperature',
+  'Humidity',
+  'Light',
+  'Radiation'
+];
+
+const Panel = (props) => (<Container>
+  {Labels.map(label => {
+    return figure(label, props[label.toLowerCase()])
+  })}
 </Container>);
 
 export default Panel;
