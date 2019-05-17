114f47debc70134d4945c47c1ef5182e09383466
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-17 22:01:48 +1000

    refactor a little bit

---
+++ src/constants/index.jsx
@@ -0,0 +1,9 @@
+import {Icon} from "antd";
+import React from "react";
+
+export const Sensors = {
+  Temperature: <Icon type="dashboard" style={{color: 'rgba(48, 197, 255, 1)'}}/>,
+  Humidity: <Icon type="cloud" />,
+  Light: <Icon type="bulb" style={{color: 'rgba(0, 167, 225, 1)'}} />,
+  Radiation: <Icon type="alert" style={{color: 'rgba(248, 51, 60, 1)'}}/>
+}
--- src/ui/Panel.jsx
+++ src/ui/Panel.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Col, Row, Icon } from 'antd';
-const format = (figure) => isNaN(Number(figure)) ? '-' : Number(figure).toFixed(2);
 
-const Sensors = {
-  Temperature: <Icon type="dashboard" style={{color: 'rgba(48, 197, 255, 1)'}}/>,
-  Humidity: <Icon type="cloud" />,
-  Light: <Icon type="bulb" style={{color: 'rgba(0, 167, 225, 1)'}} />,
-  Radiation: <Icon type="alert" style={{color: 'rgba(248, 51, 60, 1)'}}/>
-}
+import {Sensors} from "../constants";
+
+const format = (figure) => isNaN(Number(figure)) ? '-' : Number(figure).toFixed(2);
 
 const figure = (type, value) => {
   return (
@@ -23,14 +19,12 @@ const figure = (type, value) => {
 };
 
 const Panel = (props) => (
-  <div>
-    <Row>
-      {Object.keys(Sensors).map(label => {
-        return (<Col span={6} key={label}>
-          {figure(label, props[label.toLowerCase()])}
-        </Col>)
-      })}
-    </Row>
-  </div>);
+  <Row>
+    {Object.keys(Sensors).map(label => {
+      return (<Col span={6} key={label}>
+        {figure(label, props[label.toLowerCase()])}
+      </Col>)
+    })}
+  </Row>);
 
 export default Panel;
