9f1d5f370a9ab7e2b9e4d314ec2a3317865960fd
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-17 19:49:46 +1000

    refactor a little bit

--- src/app.js
+++ src/app.js
@@ -1,27 +1,6 @@
-import {parse} from './parser';
-import Averager from './averager';
+import React from "react";
+import ReactDOM from "react-dom";
+import AppContainer from './ui/AppContainer';
 
-const pubnub = new PubNub({
-  subscribeKey: 'sub-c-5f1b7c8e-fbee-11e3-aa40-02ee2ddab7fe'
-});
-
-pubnub.subscribe({
-  channels: ["pubnub-sensor-network"]
-});
-
-const THE_MAP = [...Array(11).keys()].reduce((previous, current) => ({
-  ...previous,
-  [`group_${current}`]: new Averager(10)
-}), {});
-
-pubnub.addListener({
-  message: function(data) {
-    const parsed = parse(data.message);
-    THE_MAP[parsed.groupName].put(parsed.readings);
-    console.log({
-      group: parsed.groupName,
-      timestamp: parsed.timestamp,
-      average: THE_MAP[parsed.groupName].average()
-    });
-  }
-});
+var mountNode = document.getElementById("app");
+ReactDOM.render(<AppContainer />, mountNode);
---
+++ src/ui/AppContainer.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import PubNubReact from 'pubnub-react';
+
+import Averager from '../utils/averager';
+import {parse} from '../utils/parser';
+
+import Panel from './Panel';
+
+const THE_MAP = [...Array(11).keys()].map(index => ({group: `group_${index}`, averager: new Averager(10)}));
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.pubnub = new PubNubReact({ subscribeKey: 'sub-c-5f1b7c8e-fbee-11e3-aa40-02ee2ddab7fe' });
+    this.pubnub.init(this);
+  }
+
+  componentWillMount() {
+    this.pubnub.subscribe({ channels: ['pubnub-sensor-network'] });
+    this.pubnub.getMessage('pubnub-sensor-network', 1);
+  }
+
+  componentWillUnmount() {
+    this.pubnub.unsubscribe({ channels: ['pubnub-sensor-network'] });
+  }
+
+  render() {
+    const message = this.pubnub.getMessage('pubnub-sensor-network');
+    if(message.length === 0) return null;
+
+    const parsed = parse(message[0].message);
+    THE_MAP.find(x => x.group === parsed.groupName).averager.put(parsed.readings);
+
+    return <div>
+      {THE_MAP.map(value => {
+        return <Panel key={value.group} {...value.averager.average()} />
+      })}
+    </div>
+  }
+}
+
+export default AppContainer;
