1aadd00346a2f6e7a110c5f17301759be33c5000
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-20 21:39:51 +1000

    separate css out from sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,11 +3,13 @@ import {Card, Col, Row} from "antd";
 import ChartSection from "./ChartSection";
 import {createRealTimeChart} from "../utils/chart";
 
+import './Sidebar.css';
+
 const Sidebar = ({groups = {}}) => (
   <Row gutter={16}>
 
     {Object.values(groups).filter(g => g.data.location).map(value => {
-      return (<Col span={24} key={value.name} style={{padding: '8px'}}>
+      return (<Col span={24} key={value.name} className='column'>
         <Card title={value.data.displayName} bordered>
           <ChartSection createRealTimeChart={createRealTimeChart} group={value.name} {...value.averager.average()} />
         </Card>
