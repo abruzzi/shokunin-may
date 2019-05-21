832ceb2561876173ece6b90ac0c5184fad233e0c
Juntao Qiu <juntao.qiu@gmail.com>
2019-05-21 20:17:23 +1000

    using logscale to show them on the same section

--- src/utils/chart.js
+++ src/utils/chart.js
@@ -1,18 +1,20 @@
+import d3 from 'd3';
 import Rickshaw from "rickshaw";
 import {COLORS} from "../constants";
 
 const updateInterval = 500;
 
 const createRealTimeChart = (id) => {
-  const lines = Object.keys(COLORS).map(type => ({name: type, color: COLORS[type]}))
+  const logScale = d3.scale.log().domain([1, 900]).range([0, 600]).clamp(true).nice();
+  const lines = Object.keys(COLORS).map(type => ({name: type, color: COLORS[type], scale: logScale}))
 
   const chart = new Rickshaw.Graph({
     element: document.querySelector(`#${id}`),
-    width: "240",
-    height: "120",
+    width: 240,
+    height: 120,
     renderer: "line",
-    min: "0",
-    max: "1000",
+    min: 1,
+    max: 900,
     series: new Rickshaw.Series.FixedDuration(
       lines,
       undefined,
